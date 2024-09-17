e4ca3a3811ee021392072994c4f9d58e5d062ae5
anshul-nan <105433706+anshul-nan@users.noreply.github.com>
2024-09-17 15:17:20 +0530

    Add files via upload

---
+++ pro.js
@@ -0,0 +1,11 @@
+function search(){
+    o=document.getElementById("input").value;
+    p=o.toLowerCase();
+    f=p.replace(" ","-")
+    link="https://olympics.com/en/athletes/"+f+"/";
+    console.log(link)
+   
+
+    document.getElementById("result").innerHTML="<section id='result'> <iframe src='' id='if'></iframe></section>"
+    document.getElementById("if").src=link;
+}
